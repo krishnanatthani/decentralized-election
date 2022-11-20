1c02ca290cd93b394b40d9c964b3b39d6a7bde6a
krishnanatthani <krish.natthani@gmail.com>
2022-11-20 21:10:10 +0530

    final updates

---
+++ contracts/elections.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.2;
+
+contract election{
+	//constructor
+	//store and readcandidate
+
+	string public candidate;
+
+	 constructor() public {
+		candidate = "candidate 1";
+	} 
+}
